cd404bfd3c51b769a003ef5fdd082e17cca140e6
lisaadnr <elisabunga63@gmail.com>
2023-12-05 21:41:04 +0700

    first commit

---
+++ app/src/main/java/com/dicoding/toekangku1/ui/chat/ChatActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.toekangku1.ui.chat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.toekangku1.R
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
---
+++ app/src/main/java/com/dicoding/toekangku1/ui/onboard/onboardActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.toekangku1.ui.onboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.toekangku1.R
+
+class onboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboard)
+    }
+}
